d1276861dea37b78ece6181df2254545bec3084a
Yavuz Demir <yavuz.demir@vr-masters.com>
2020-05-09 12:45:15 +0300

    Transform logger added

---
+++ Assets/Scripts/TransformLogger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransformLogger : MonoBehaviour
+{
+    public Transform[] transforms;
+    public Text text;
+
+    void Update()
+    {
+        string s = "";
+        foreach(var t in transforms) {
+            s += t.name + " Pos: " + t.localPosition + " Rot:" + t.localRotation.eulerAngles + "\n";
+        }
+        text.text = s;
+    }
+}
